e605ffa00b3cb1852914c07bb6f8ae08783236cd
jadekang <jkang.se@gmail.com>
2020-11-13 00:16:43 +0900

    Built a custom carousel and edited button locations with media query

--- src/components/Custom/CustomCard/index.js
+++ src/components/Custom/CustomCard/index.js
@@ -5,15 +5,19 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   root: {
     marginRight: theme.spacing(3),
-    padding: '0 8px',
+    // padding: '0 8px',
   },
   style: {
     width: '384px',
     height: '320px',
     borderRadius: '10px',
     boxShadow: '0px 4px 8px rgba(8, 35, 48, 0.24)',
+    [theme.breakpoints.between('md', 'lg')]: {
+      width: '100%',
+      height: '238px',
+    },
     [theme.breakpoints.between('xs', 'sm')]: {
-      width: '286px',
+      width: '100%',
       height: '238px',
     },
   },
@@ -28,6 +32,7 @@ const useStyles = makeStyles((theme) => ({
   },
   category: {
     color: '#535353',
+    marginRight: '4px',
   },
   imageHolder: {
     padding: 0,
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -13,6 +13,10 @@ import RightButton from './RightButton.svg';
 //Custom Component
 import CustomCard from '../Custom/CustomCard';
 
+// Multiple Images Slider
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
 const ProjectImageGroup = [
   ProjectImg1,
   ProjectImg2,
@@ -53,9 +57,13 @@ const useStyles = makeStyles((theme) => ({
     // justifyContent: 'space-between',
   },
   circleButton: {
-    [theme.breakpoints.between('xs', 'sm')]: {
+    '@media (max-width:600px) and (min-width: 500px)': {
       width: 30,
       height: 30,
+    },
+    '@media (max-width:800px) and (min-width: 601)': {
+      width: 50,
+      height: 50,
     },
   },
   sliderButtons: {
@@ -68,45 +76,78 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: 24,
     padding: 8,
   },
+  skill: {
+    marginRight: '4px',
+  },
+  customButtonGroup: {
+    position: 'relative',
+    bottom: '410px',
+    left: 0,
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    right: 0,
+    '@media (min-width:500px)': {
+      bottom: '420px',
+      left: 0,
+      right: 0,
+    },
+    '@media (max-width:600px)': {
+      bottom: '405px',
+      left: 0,
+      right: 0,
+    },
+    '@media (min-width:601px)': {
+      bottom: '450px',
+      left: 0,
+      right: 0,
+    },
+    '@media (min-width: 801px) and (max-width:959px)': {
+      bottom: '420px',
+    },
+    '@media (min-width: 960px) and (max-width:1005px)': {
+      bottom: '450px',
+    },
+    '@media (min-width: 1006px) and (max-width:1024px)': {
+      bottom: '420px',
+    },
+    '@media (min-width: 1025px) and (max-width:1340px)': {
+      bottom: '440px',
+    },
+    '@media (min-width: 1340px) and (max-width:1440px)': {
+      bottom: '420px',
+    },
+  },
 }));
-
-const CardContainer = () => {
-  const classes = useStyles();
-  return (
-    <div className={classes.cardBox}>
-      {MyData.projects.map((item, id) => {
-        console.log(MyData.projects.length);
-        return (
-          <>
-            <CustomCard
-              key={item.id}
-              className={classes.card}
-              image={ProjectImageGroup[0]}
-              name={item.name}
-              role={item.role}
-              skills={item.skills.map((item, index) => {
-                return <Typography variant="body2">{item.skill}</Typography>;
-              })}
-            />
-          </>
-        );
-      })}
-    </div>
-  );
-};
 
 const Projects = () => {
   const classes = useStyles();
 
-  const [current, setCurrent] = useState(0);
-
-  const prevButtonHandler = (e) => {
-    console.log(e.target.name);
+  const CustomButtonGroupAsArrows = ({ next, previous }) => {
+    return (
+      <div className={classes.customButtonGroup}>
+        <Button className={classes.sliderButtons}>
+          <img
+            src={LeftButton}
+            alt="left-button"
+            className={classes.circleButton}
+            name="prevButton"
+            onClick={previous}
+          />
+        </Button>
+        <Button className={classes.sliderButtons}>
+          <img
+            src={RightButton}
+            alt="left-button"
+            className={classes.circleButton}
+            name="nextButton"
+            onClick={next}
+          />
+        </Button>
+      </div>
+    );
   };
 
-  const nextButtonHandler = (e) => {
-    console.log(e.target.name);
-  };
   return (
     <div className={classes.root}>
       <div className={classes.titleBox}>
@@ -120,7 +161,7 @@ const Projects = () => {
           </Typography>
         </Grid>
         <Grid item s={4}>
-          <Button className={classes.sliderButtons} onClick={prevButtonHandler}>
+          {/* <Button className={classes.sliderButtons}>
             <img
               src={LeftButton}
               alt="left-button"
@@ -129,18 +170,91 @@ const Projects = () => {
             />
           </Button>
 
-          <Button className={classes.sliderButtons} onClick={nextButtonHandler}>
+          <Button className={classes.sliderButtons}>
             <img
               src={RightButton}
               alt="left-button"
               className={classes.circleButton}
               name="nextButton"
             />
-          </Button>
+          </Button> */}
         </Grid>
       </Grid>
 
-      <CardContainer />
+      <div className={classes.cardBox}></div>
+
+      <Carousel
+        additionalTransfrom={0}
+        arrows={false}
+        autoPlaySpeed={3000}
+        centerMode={false}
+        className=""
+        containerClass="container"
+        customButtonGroup={<CustomButtonGroupAsArrows />}
+        dotListClass=""
+        draggable
+        focusOnSelect={false}
+        infinite={false}
+        itemClass=""
+        keyBoardControl
+        minimumTouchDrag={80}
+        renderButtonGroupOutside
+        renderDotsOutside={false}
+        responsive={{
+          desktop: {
+            breakpoint: {
+              max: 3000,
+              min: 1024,
+            },
+            items: 3,
+            partialVisibilityGutter: 40,
+          },
+          mobile: {
+            breakpoint: {
+              max: 464,
+              min: 0,
+            },
+            items: 1,
+            partialVisibilityGutter: 30,
+          },
+          tablet: {
+            breakpoint: {
+              max: 1024,
+              min: 464,
+            },
+            items: 2,
+            partialVisibilityGutter: 30,
+          },
+        }}
+        showDots={false}
+        sliderClass=""
+        slidesToSlide={2}
+        swipeable
+      >
+        {MyData.projects.map((item, id) => {
+          return (
+            <div key={item.id}>
+              <CustomCard
+                className={classes.card}
+                image={ProjectImageGroup[0]}
+                name={item.name}
+                role={item.role}
+                skills={item.skills.map((item, index) => {
+                  return (
+                    <Typography
+                      variant="body2"
+                      key={index}
+                      className={classes.skill}
+                    >
+                      {item.skill + ', '}
+                    </Typography>
+                  );
+                })}
+              />
+            </div>
+          );
+        })}
+      </Carousel>
     </div>
   );
 };
